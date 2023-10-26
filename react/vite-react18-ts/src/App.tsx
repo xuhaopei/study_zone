91f30e040415b035bded3f23537367473acc2627
inke_xuhapei <xuhaopei@inke.cn>
2023-10-27 00:48:47 +0800

    feat:window版本的配置完成

--- react/vite-react18-ts/src/App.tsx
+++ react/vite-react18-ts/src/App.tsx
@@ -2,9 +2,10 @@ import { useDeferredValue, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import * as React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(1)
   const deferredValue = useDeferredValue(count)
   return (
     <>
