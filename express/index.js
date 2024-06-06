9de9e6bbc7f1cd8274763ba525248275bf2c3df4
opao <xuhaopei@inke.cn>
2024-06-06 11:49:03 +0800

    feat

--- express/index.js
+++ express/index.js
@@ -52,7 +52,28 @@ app.get('/b',(req,res)=>{
   console.log('/b',req.query)
   res.send('hhhh')
 })
-const port = 9000
+
+// 动态生成webmanifest文件，根据参数进行动态变化。
+app.get('/askWebmanifest',(req,res)=>{
+  const stringData = JSON.stringify({
+    name: "Pooke2",
+    short_name: "Pooke",
+    icons: [
+      {
+        src:"https://upload.wikimedia.org/wikipedia/commons/5/5c/Bml_x_512_y_512_p_31_iterated_32000.png",
+        type: "image/png",
+        sizes: "512x512"
+      }
+   ],
+     theme_color: "#232227",
+     background_color: "#232227",
+     start_url: `./?op=${req.query['op']}`,
+     display: "standalone"
+ })
+  res.setHeader('Content-Type', 'application/manifest+json');
+  res.send(stringData);
+})
+const port = 9001
 app.listen(port, () => {
     console.log(`http://${getIPAdress()}:${port}`)
 })
